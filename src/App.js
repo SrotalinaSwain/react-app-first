3a39ca017924857748c3b52322af7d98372718f6
srotalina <52050791+srotalina@users.noreply.github.com>
2022-05-28 03:00:19 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,31 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Start deploying basic apps from React to Git repos using Github pages...#HappyCoding
+         Steps:
+         1. npm install gh-pages --save-dev
+
+         2.Add properties to package.json file.
+            "homepage": "https://SrotalinaSwain.github.io/react-app-first"
+
+         3.Add properties to"scripts" in package.json file: 
+
+         "predeploy": "npm run build",
+         "deploy": "gh-pages -d build"
+
+         4.Run these on command line:
+          
+          git init
+          git remote add origin https://github.com/SrotalinaSwain/react-app-first.git
+          npm run deploy
+
+         5.For master file :
+
+          git add .
+          git commit -m "Your awesome message"
+          git push origin master
+
+
+          #HappyCoding
         </a>
       </header>
     </div>
