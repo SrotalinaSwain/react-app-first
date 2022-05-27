0f19df775c3ea7e84c82fcbdc92aa95098e772dc
srotalina <52050791+srotalina@users.noreply.github.com>
2022-05-27 18:22:43 +0530

    Deploying now

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Srota$ouls
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Start deploying basic apps from React to Git repos using Github pages...#HappyCoding
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
